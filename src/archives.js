17e654a3e7c56b671617c09fca12774174400442
Ethan P <eth-p+git@hidden.email>
2022-07-22 22:42:28 -0700

    archive: Add 2022 to archive

--- src/archives.js
+++ src/archives.js
@@ -2,7 +2,11 @@ const archives = [
 	{
 		year: 2021,
 		site: "https://stormhacks-archive-2021-internal.netlify.app/",
-	}
+	},
+	{
+		year: 2022,
+		site: "https://stormhacks-archive-2022-internal.netlify.app/",
+	},
 ];
 
 export default archives.sort((a, b) => a.year - b.year);
