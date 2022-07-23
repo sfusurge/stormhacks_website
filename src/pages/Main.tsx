6019f58d20c2b4ff6d35c79938e5435a33651200
Ethan P <eth-p+git@hidden.email>
2022-07-22 22:46:59 -0700

    archive: Make links to each year's archive

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -6,8 +6,19 @@ export interface MainProps {
 
 export default function Main(props: MainProps) {
 	return (
-		<div>
-			TODO
+		<div style={{padding: '1em'}}>
+			<p>This is a placeholder page, while I wait for the designers to come up with something.</p>
+			<p>-- Ethan</p>
+			<hr/>
+			<ul>
+				{props.archives.map(archive => {
+					return (
+						<li>
+							<a href={`/${archive.year}`} key={archive.year}>StormHacks {archive.year} Website</a>
+						</li>
+					)
+				})}
+			</ul>
 		</div>
 	);
 }
