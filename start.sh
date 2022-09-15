4762c4dfc7cd3590298a0db8354ea9ea582f7af7
Ethan P <eth-p+git@hidden.email>
2022-09-14 21:37:02 -0700

    meta: Improve automatic `nvm` support
    Now it looks for nvm in multiple locations and automatically removes the
    `.npmrc` prefix, which was causing issues.

--- start.sh
+++ start.sh
@@ -3,15 +3,42 @@ set -euo pipefail
 # Initial setup script.
 # This can be used instead of the instructions for simplicity.
 
+# Remove the prefix from .npmrc temporarily.
+if [ -f "$HOME/.npmrc" ]; then
+	if grep "^prefix=" "$HOME/.npmrc" &>/dev/null; then
+		echo "Your .npmrc file has a prefix. It will be removed while this script is running."
+
+		# Create a backup of the .npmrc file.
+		cp "$HOME/.npmrc" "$HOME/.npmrc.if_i_exist_replace_original_with_me"
+		trap 'mv "$HOME/.npmrc.if_i_exist_replace_original_with_me" "$HOME/.npmrc"' EXIT
+
+		# Remove the prefix from the original .npmrc file.
+		grep -v "^prefix=" \
+			< "$HOME/.npmrc.if_i_exist_replace_original_with_me" \
+			> "$HOME/.npmrc"
+	fi
+fi
+
 # Use the NodeJS version specified in .nvmrc, if it exists.
 # If not already downloaded, this will download NodeJS.
 if [ -f ".nvmrc" ]; then
 	if ! type nvm &>/dev/null; then
-		if [ -f "$HOME/.nvm/nvm.sh" ]; then
-			set +euo pipefail # <-- nvm doesn't like `-euo pipefail`.
-			source "$HOME/.nvm/nvm.sh"
-			set -euo pipefail
-		else
+		nvm_locations=(
+			"$HOME/.nvm/nvm.sh"
+			"/usr/local/share/nvm/init-nvm.sh"
+			"/usr/share/nvm/init-nvm.sh"
+		)
+
+		for loc in "${nvm_locations[@]}"; do
+			if [ -f "$loc" ]; then
+				set +euo pipefail # <-- nvm doesn't like `-euo pipefail`.
+				source "$loc"
+				set -euo pipefail
+				break
+			fi
+		done
+
+		if ! type nvm &>/dev/null; then
 			echo "Please install nvm (Node Version Manager):"
 			echo "  https://github.com/nvm-sh/nvm#installing-and-updating"
 			exit 1
