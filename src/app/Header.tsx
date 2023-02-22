47573edaffa25db51abd59dd83a380998a7e3ab2
Ethan P <eth-p+git@hidden.email>
2023-02-22 00:04:55 -0800

    2023: Basic header work

--- src/app/Header.tsx
+++ src/app/Header.tsx
@@ -1,7 +1,15 @@
+import cx from "classnames";
+
+import { useEffect, useRef } from "react";
 import { useTranslation } from "react-i18next";
 import { Link } from "react-router-dom";
 
+import InlineSVG from "~/SVG";
 import SurgeSocialLinks from "~/SurgeSocialLinks";
+
+import { ReactComponent as StormhacksIcon } from "$asset/icon/stormhacks.svg";
+
+import Styles from "./Header.module.scss";
 
 /**
  * Navigation links.
@@ -10,10 +18,31 @@ function NavLinks() {
 	const { t } = useTranslation();
 
 	return (
-		<>
+		<nav className={Styles.linkContainer}>
 			<Link to={"/sponsors"}>{t("header.link.sponsors")}</Link>
 			<Link to={"/faq"}>{t("header.link.faq")}</Link>
-		</>
+			<Link to={"/about-us"}>{t("header.link.about-us")}</Link>
+			<a href="about:blank">A Long Link</a>
+			<a href="about:blank">Even More Links</a>
+		</nav>
+	);
+}
+
+function NavLinkToMain() {
+	return (
+		<nav className={Styles.headerStickyLeft}>
+			<Link to={"/"} className={Styles.logoLink}>
+				<InlineSVG svg={StormhacksIcon} />
+			</Link>
+		</nav>
+	);
+}
+
+function HeaderApplyButton() {
+	return (
+		<div className={Styles.headerStickyRight}>
+			<button>Apply</button>
+		</div>
 	);
 }
 
@@ -21,12 +50,41 @@ function NavLinks() {
  * The header for the StormHacks 2023 website.
  */
 function Header() {
+	const headerRef = useRef<HTMLElement>(null);
+
+	useEffect(() => {
+		console.log("REF", headerRef);
+		const headerEl = headerRef.current;
+		if (headerEl == null) return;
+
+		const refreshScrollableAttributes = () => {
+			const isScrolledLeft = headerEl.scrollLeft <= 0;
+			const isScrolledRight = headerEl.scrollLeft >= headerEl.scrollWidth - headerEl.clientWidth - 1;
+			headerEl.setAttribute("data-scrollable-left", isScrolledLeft ? "no" : "yes");
+			headerEl.setAttribute("data-scrollable-right", isScrolledRight ? "no" : "yes");
+		};
+
+		// Update the `data-scrollable-left` and `data-scrollable-right` attributes.
+		// On mobile, these will be used to change the styles to indicate that there's more to the header.
+		refreshScrollableAttributes();
+		const listener = () => requestAnimationFrame(refreshScrollableAttributes);
+
+		// Register the listener and return a function to unregister the listener.
+		headerEl.addEventListener("scroll", listener, { passive: true });
+		return () => headerEl.removeEventListener("scroll", listener);
+	}, [headerRef]);
+
 	return (
-		<header>
-			<nav>
+		<header className={cx(Styles.header, "width-limited")} ref={headerRef}>
+			<div className={Styles.headerContents}>
+				<NavLinkToMain />
 				<NavLinks />
-			</nav>
-			<SurgeSocialLinks />
+				<div className={Styles.spacer} />
+				<div className={Styles.linkContainer}>
+					<SurgeSocialLinks />
+				</div>
+				<HeaderApplyButton />
+			</div>
 		</header>
 	);
 }
--- src/index.tsx
+++ src/index.tsx
@@ -40,6 +40,38 @@ root.render(
 	</React.StrictMode>
 );
 
+// Listen for scroll events.
+// This is used for styling.
+let lastScrollY = 0;
+let lastScrollAtTouch = 999999; /* Show the header at page load. */
+window.addEventListener("touchstart", () => (lastScrollAtTouch = window.scrollY));
+window.addEventListener(
+	"scroll",
+	() => {
+		requestAnimationFrame(() => {
+			const body = document.body;
+
+			const scrollY = window.scrollY;
+			const deltaScrollY = scrollY - lastScrollY;
+			lastScrollY = scrollY;
+
+			// Calculate scroll direction.
+			body.setAttribute("data-scroll-direction", deltaScrollY < 0 ? "up" : "down");
+
+			// Calculate scroll position.
+			const bottomScrollY = body.clientHeight - window.innerHeight;
+			body.setAttribute(
+				"data-scroll-position",
+				scrollY <= 0 ? "top" : scrollY >= bottomScrollY ? "bottom" : "scrolled"
+			);
+
+			// Calculate scroll delta.
+			body.style.setProperty("--scroll-offset-since-touch", `${scrollY - lastScrollAtTouch}px`);
+		});
+	},
+	{ passive: true }
+);
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
