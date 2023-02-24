cec046d7b01f44bf99f43995136d0a1ee5783cde
Ethan P <eth-p+git@hidden.email>
2023-02-23 19:56:54 -0800

    2023: Refactor event listeners for header scroll

--- src/app/Header.tsx
+++ src/app/Header.tsx
@@ -1,7 +1,8 @@
 import cx from "classnames";
 
-import { useEffect, useRef } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 
+import { getScrollParent, isAnimatedScrolling } from "~/ScrollAnchor/util";
 import SurgeSocialLinks from "~/SurgeSocialLinks";
 
 import Styles from "./Header.module.scss";
@@ -14,6 +15,7 @@ import { AppRoute } from "./Routes";
 function Header({ appRoutes }: { appRoutes: AppRoute[] }) {
 	const headerRef = useRef<HTMLElement>(null);
 
+	// Register listeners for showing scroll indicators when on mobile layout.
 	useEffect(() => {
 		const headerEl = headerRef.current;
 		if (headerEl == null) return;
@@ -35,6 +37,36 @@ function Header({ appRoutes }: { appRoutes: AppRoute[] }) {
 		return () => headerEl.removeEventListener("scroll", listener);
 	}, [headerRef]);
 
+	// Register listeners for showing/hiding the header when scrolling on mobile.
+	const [scrollInfo] = useState<ScrollListenersSharedState>({
+		lastScrollY: 0,
+		lastScrollAtTouch: null,
+	});
+
+	useEffect(() => {
+		const headerEl = headerRef.current;
+		if (headerEl == null) return;
+
+		const scrollTarget = getScrollParent(headerEl);
+		if (scrollTarget == null) return;
+
+		const [listenTarget, getScrollY] =
+			scrollTarget?.nodeName === "HTML"
+				? [window, () => window.scrollY]
+				: [scrollTarget, () => scrollTarget.scrollTop];
+
+		const onTouch = createHeaderTouchCallback(headerEl, scrollTarget, getScrollY, scrollInfo);
+		const onScroll = createHeaderScrollCallback(headerEl, scrollTarget, getScrollY, scrollInfo);
+
+		listenTarget.addEventListener("touchstart", onTouch);
+		listenTarget.addEventListener("scroll", onScroll);
+
+		return () => {
+			listenTarget.removeEventListener("touchstart", onTouch);
+			listenTarget.removeEventListener("scroll", onScroll);
+		};
+	}, [headerRef, scrollInfo]);
+
 	return (
 		<header className={Styles.header} ref={headerRef} id="header">
 			<div className={cx(Styles.headerContents, "width-limited")}>
@@ -47,6 +79,70 @@ function Header({ appRoutes }: { appRoutes: AppRoute[] }) {
 			</div>
 		</header>
 	);
+}
+
+type ScrollListenersSharedState = {
+	lastScrollY: number;
+	lastScrollAtTouch: number | null;
+};
+
+function createHeaderScrollCallback(
+	el: HTMLElement,
+	scrollEl: HTMLElement,
+	getScrollY: () => number,
+	state: ScrollListenersSharedState
+): () => void {
+	let queueId = 0;
+	let scrollDelta = 0;
+
+	return () => {
+		const scrollY = getScrollY();
+		const isAnimating = isAnimatedScrolling(scrollEl);
+
+		const myQueueId = ++queueId;
+		window.requestAnimationFrame(() => {
+			// Debounce.
+			if (myQueueId !== queueId) return;
+			const { lastScrollY } = state;
+			state.lastScrollY = scrollY;
+
+			// Apply classes.
+			el.setAttribute("data-scrolling", scrollY >= state.lastScrollY ? "down" : "up");
+			el.classList.toggle(Styles.forceVisible, isAnimating);
+			el.classList.toggle(Styles.atContainerTop, scrollY <= 0);
+
+			// Stop if animating.
+			if (isAnimating) {
+				if (state.lastScrollAtTouch != null) {
+					state.lastScrollAtTouch = scrollY;
+					scrollDelta = 0;
+				}
+
+				return;
+			}
+
+			// If on mobile and a touch event has been received, hide/show the header.
+			if (state.lastScrollAtTouch !== null) {
+				const headerHeight = el.clientHeight;
+				const oldDelta = scrollDelta;
+				let newDelta = oldDelta + (scrollY - lastScrollY);
+
+				scrollDelta = Math.max(Math.min(newDelta, headerHeight), -headerHeight);
+				el.style.setProperty("--header-hide-offset", `${scrollDelta}px`);
+			}
+		});
+	};
+}
+
+function createHeaderTouchCallback(
+	_el: HTMLElement,
+	_scrollEl: HTMLElement,
+	getScrollY: () => number,
+	state: ScrollListenersSharedState
+): () => void {
+	return () => {
+		state.lastScrollAtTouch = getScrollY();
+	};
 }
 
 function HeaderApplyButton() {
