619408fd8c006c7d239dd4014a30f6a679a88db8
Ethan P <eth-p+git@hidden.email>
2023-05-02 18:42:16 -0700

    2023: Update "About Surge" section

--- src/pages/AboutSurge.tsx
+++ src/pages/AboutSurge.tsx
@@ -6,6 +6,10 @@ import { useTranslation } from "react-i18next";
 import ExecPhoto from "~/ExecPhoto";
 import Page from "~/Page";
 
+import { ReactComponent as HeartGraphic } from "$asset/giant-heart.svg";
+import { ReactComponent as OtterLeft } from "$asset/otter-left.svg";
+import { ReactComponent as OtterRight } from "$asset/otter-right.svg";
+
 import Styles from "./AboutSurge.module.scss";
 
 const SurgeExecs = SurgeInfo.execs;
@@ -17,17 +21,15 @@ const SurgeExecs = SurgeInfo.execs;
  */
 function AboutSurgePage() {
 	return (
-		<Page className="width-limited">
+		<Page className={cx("width-limited", Styles.page)}>
 			<AboutSurgeSection />
 		</Page>
 	);
 }
 
 export function AboutSurgeSection() {
-	const { t } = useTranslation();
 	return (
 		<article className={Styles.container}>
-			<h1>{t("about-surge.title")}</h1>
 			<SectionAbout />
 			<SectionExecs />
 		</article>
@@ -38,8 +40,17 @@ function SectionAbout() {
 	const { t } = useTranslation();
 	return (
 		<div className={cx(Styles.section, Styles.about)}>
-			<p>{t("about-surge.paragraph.0")}</p>
-			<p>{t("about-surge.paragraph.1")}</p>
+			<OtterLeft className={Styles.otterLeft} />
+			<OtterRight className={Styles.otterRight} />
+			<div className={Styles.graphic}>
+				<HeartGraphic />
+				<HeartGraphic className={Styles.blurred} />
+			</div>
+			<div className={Styles.description}>
+				<h1>{t("about-surge.title")}</h1>
+				<p>{t("about-surge.paragraph.0")}</p>
+				<p>{t("about-surge.paragraph.1")}</p>
+			</div>
 		</div>
 	);
 }
