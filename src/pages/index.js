8be8f7285ed1a62fdcf2f7a648d7d9c86813d9c5
ChrisFong604 <ccffoonngg@gmail.com>
2024-03-20 17:40:32 -0700

    configured next-i18n, removed app router in favour of using pages system. changed pages to JS

---
+++ src/pages/index.js
@@ -0,0 +1,18 @@
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+
+export async function getStaticProps({ locale }) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ["common", "footer"])),
+      // Will be passed to the page component as props
+    },
+  };
+}
+
+export default function Home() {
+  return (
+    <>
+      <p>Stormhacks 2024 is coming!</p>
+    </>
+  );
+}
