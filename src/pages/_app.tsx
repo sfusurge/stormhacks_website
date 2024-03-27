32fc9add3f1ed776c6522e3d7ea5a3aae2cc5083
ChrisFong604 <ccffoonngg@gmail.com>
2024-03-27 14:35:16 -0700

    added jsconfig for tsserver LSP, fixed tailwind config, moved global css to src root and imported in app root

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import type { AppProps } from "next/app";
 import { appWithTranslation } from "next-i18next";
 
+import "../globals.css";
+
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
