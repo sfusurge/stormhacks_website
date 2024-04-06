d13ccadb9766646bf3a1722557fce61bf5b91c23
ayana <ayanahye2@gmail.com>
2024-04-05 20:59:41 -0700

    Fix navbar and add the mlh icon

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import PlaceHoldImage2 from "../../public/images/placehold2-img.png";
 import Image from 'next/image';
+import Badge from "../../public/images/mlh.svg";
 
 function NavBar() {
 
     return (
-        <div className='pb-5 flex justify-between pt-5 bg-customPurple text-white'>
-            <ul className='flex ml-5 items-center'>
+        <div>
+        <div className='pb-5 flex justify-between pt-5 bg-customPurple text-white ' style={{ position: 'fixed', top: 0, width: '85vw', zIndex: 100 }}>
+            <ul className='flex items-center ' >
                 <Image width={40} src={PlaceHoldImage2}/>
               
                 <li className='ml-5 mr-5 hover:underline'>Home</li>
@@ -15,12 +17,17 @@ function NavBar() {
                 <li className='mr-5 hover:underline'>FAQ</li>
                 <li className='mr-5 hover:underline'>Surge</li>
             </ul>
-            <ul className='flex mr-5'>
+            <ul className='flex mr-28'>
                 <Image width={40} src={PlaceHoldImage2}/>
                 <Image width={40} src={PlaceHoldImage2}/>
                 <Image width={40} src={PlaceHoldImage2}/>
                 <Image width={40} src={PlaceHoldImage2}/>
             </ul>
+            
+        </div>
+        <div className='absolute top-0 right-0 mr-5 mt-5' style={{ zIndex: 101 }}>
+            <Image width={80} src={Badge} />
+        </div>
         </div>
     )
 }
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -3,6 +3,7 @@ import NavBar from "../components/NavBar";
 import PlaceHoldImage from "../../public/images/placehold-img.png";
 import NavIcon from "../../public/images/navicon-img.png";
 import Image from 'next/image';
+import Badge from "../../public/images/mlh.svg";
 
 function Landing() {
 
@@ -31,27 +32,39 @@ function Landing() {
     };
 
     return (
-        <div className='bg-customPurple'>
+        <div className='flex items-center justify-center bg-customPurple'>
+            <div className={`${isSmallScreen ? '' : 'w-[80vw]'} `}>
             {!isSmallScreen && <NavBar />}
             {isSmallScreen && 
-                <div className='pl-5 pt-5 bg-customPurple'>
-                <button onClick={toggleNav}><Image src={NavIcon} height={40}/></button>
+                
+                <div className=' pt-5 bg-customPurple' style={{ position: 'fixed', top: 0, width: '100vw', zIndex: 100 }}>
+                <button className='pl-5' onClick={toggleNav}><Image src={NavIcon} height={40}/></button>
+
                 {showNav && 
-                    <ul className=' items-center text-white'>
-                        <li className='text-customPurple border text-white py-1 px-4 rounded mt-5 mr-5 text-center hover:bg-white hover:text-customPurple'>Home</li>
-                        <li className='text-customPurple border text-white py-1 px-4 rounded mt-1 mr-5 text-center hover:bg-white hover:text-customPurple'>About</li>
-                        <li className='text-customPurple border text-white py-1 px-4 rounded  mt-1 mr-5 text-center hover:bg-white hover:text-customPurple'>Sponsors</li>
-                        <li className='text-customPurple border text-white py-1 px-4 rounded  mt-1 mr-5 text-center hover:bg-white hover:text-customPurple'>FAQ</li>
-                        <li className='text-customPurple border text-white py-1 px-4 rounded  mt-1 mr-5 text-center hover:bg-white hover:text-customPurple'>Surge</li>
-                    </ul>
+                  
+                    <div className='h-[95vh] flex items-center justify-center bg-customPurple'>
+                        <ul className=' h-[40vh] items-center text-white w-[80vw]' >
+                            <li className='text-customPurple border text-white py-1 px-4 rounded mt-1  text-center hover:bg-white hover:text-customPurple'>Home</li>
+                            <li className='text-customPurple border text-white py-1 px-4 rounded mt-2  text-center hover:bg-white hover:text-customPurple'>About</li>
+                            <li className='text-customPurple border text-white py-1 px-4 rounded  mt-2 text-center hover:bg-white hover:text-customPurple'>Sponsors</li>
+                            <li className='text-customPurple border text-white py-1 px-4 rounded  mt-2 text-center hover:bg-white hover:text-customPurple'>FAQ</li>
+                            <li className='text-customPurple border text-white py-1 px-4 rounded  mt-2 text-center hover:bg-white hover:text-customPurple'>Surge</li>
+                        </ul>
+                        
+                    </div>
+    
+                 
                 }
             </div>
             }
             
-            <div className={`${isSmallScreen ? 'items-start mt-20' : 'items-center h-[40vh]'} pl-5 flex flex-col bg-customPurple justify-center text-white`}>
+            <div className={`${isSmallScreen ? 'items-start pt-32' : 'items-center h-[40vh] pt-24'} pl-5 flex flex-col bg-customPurple justify-center text-white`}>
+                <div className='absolute top-0 right-0 mr-5 mt-5' style={{ zIndex: 101 }}>
+                            <Image width={80} src={Badge} />
+                </div>
                 <h2>May 18–19, 2024 @ sfu burnaby</h2>
                 <h1 className='text-5xl mt-1 mb-5'>StormHacks 2024</h1>
-                <p className='text-xs justify-center'>Lorem ipsum dolor sit amet consectetur adipiscing elit lorem ipsum dolor sit amet consectetur adipiscing elit.</p>
+                <p className={`${isSmallScreen ? '' : 'text-center'} text-xs justify-center max-w-sm`}>Lorem ipsum dolor sit amet consectetur adipiscing elit lorem ipsum dolor sit amet consectetur adipiscing elit.</p>
                 <div className='mt-6'>
                 
                     <button className={`${isSmallScreen ? 'block w-[90vw] mb-5' : ''} border border-transparent hover:border-customPurple text-customPurple bg-white  py-2 px-4 rounded mr-5`}>
@@ -65,7 +78,7 @@ function Landing() {
             <div className='bg-customPurple h-[51vh] flex justify-center items-center text-white'>
                 <Image src={PlaceHoldImage} width={600} alt="image"/>
             </div>
-            <div className='pt-10 bg-customPurple w-[100vw] flex justify-center'>
+            <div className='pt-10 bg-customPurple flex justify-center'>
                 <div className={`w-[70vw] ${isSmallScreen ? '': 'flex'} bg-customPurple text-center justify-around mb-10`}>
                     <div className='flex flex-col text-white mb-5'>
                         <p className='text-5xl mb-2'>24</p>
@@ -86,6 +99,7 @@ function Landing() {
                     
 
                 </div>
+            </div>
             </div>
         </div>
     )
