6f6a7add1679145565abb6a7f6fa797353e6de42
ChrisFong604 <ccffoonngg@gmail.com>
2024-04-13 02:54:37 -0700

    moved desktop/mobile navbar logic to navbar component

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,17 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import NavBar from "../components/NavBar";
 import PlaceHoldImage from "../../public/images/placehold-img.png";
-import NavIcon from "../../public/images/navicon-img.png";
 import Image from 'next/image';
 
 function Landing() {
 
-    const [isSmallScreen, setIsSmallScreen] = useState(false);
+    const [isMobileScreen, setIsMobileScreen] = useState(false);
     const [showNav, setShowNav] = useState(false);
 
     useEffect(() => {
         const handleResize = () => {
-            setIsSmallScreen(window.innerWidth < 768);
+            setIsMobileScreen(window.innerWidth < 768);
         };
 
     
@@ -32,43 +31,20 @@ function Landing() {
 
     return (
         <>
-            {!isSmallScreen && <NavBar />}
-            {isSmallScreen && 
-                
-                <div className='w-full  pt-5 [background:linear-gradient(180deg,_#241b38,_#231f20)]' style={{ position: 'fixed', top: 0, width: '', zIndex: 100 }}>
-                <button className='' onClick={toggleNav}><Image src={NavIcon} height={40}/></button>
-
-                {showNav && 
-                  
-                    <div className=' h-[95vh] flex items-center justify-center items-center '>
-                        <ul className='pr-14 w-[80%] h-[40vh] text-white ' >
-                            <li className='text-customPurple border text-white py-1 px-4 rounded mt-1  text-center hover:bg-white hover:text-customPurple'>Home</li>
-                            <li className='text-customPurple border text-white py-1 px-4 rounded mt-2  text-center hover:bg-white hover:text-customPurple'>About</li>
-                            <li className='text-customPurple border text-white py-1 px-4 rounded  mt-2 text-center hover:bg-white hover:text-customPurple'>Sponsors</li>
-                            <li className='text-customPurple border text-white py-1 px-4 rounded  mt-2 text-center hover:bg-white hover:text-customPurple'>FAQ</li>
-                            <li className='text-customPurple border text-white py-1 px-4 rounded  mt-2 text-center hover:bg-white hover:text-customPurple'>Surge</li>
-                        </ul>
-                        
-                    </div>
-    
-                 
-                }
-            </div>
-            }
-            
+            <NavBar isMobileScreen={isMobileScreen} />
             <div>
-                <div className={`${isSmallScreen ? 'items-start pt-32' : 'items-center h-[40vh] pt-24 pl-5'} flex flex-col bg-customPurple justify-center text-white`}>
+                <div className={`${isMobileScreen ? 'items-start pt-32' : 'items-center h-[40vh] pt-24 pl-5'} flex flex-col bg-customPurple justify-center text-white`}>
 
                 
                     <h2>May 18–19, 2024 @ sfu burnaby</h2>
                     <h1 className='text-5xl mt-1 mb-5 '>StormHacks 2024</h1>
-                    <p className={`${isSmallScreen ? '' : 'text-center'} text-xs justify-center max-w-sm`}>Lorem ipsum dolor sit amet consectetur adipiscing elit lorem ipsum dolor sit amet consectetur adipiscing elit.</p>
-                    <div className={`${isSmallScreen ? 'w-full' : ''} mt-6 `}>
+                    <p className={`${isMobileScreen ? '' : 'text-center'} text-xs justify-center max-w-sm`}>Lorem ipsum dolor sit amet consectetur adipiscing elit lorem ipsum dolor sit amet consectetur adipiscing elit.</p>
+                    <div className={`${isMobileScreen ? 'w-full' : ''} mt-6 `}>
                     
-                        <button className={`${isSmallScreen ? 'w-full mb-5 min-w-[200px]' : 'mr-5'} border border-transparent hover:border-customPurple text-customPurple bg-white  py-2 px-4 rounded`}>
+                        <button className={`${isMobileScreen ? 'w-full mb-5 min-w-[200px]' : 'mr-5'} border border-transparent hover:border-customPurple text-customPurple bg-white  py-2 px-4 rounded`}>
                             Register as hacker
                         </button>
-                        <button className={`${isSmallScreen ? 'w-full mb-10 min-w-[200px]' : '' } border hover:bg-white hover:text-customPurple text-white py-2 px-4 rounded`}>
+                        <button className={`${isMobileScreen ? 'w-full mb-10 min-w-[200px]' : '' } border hover:bg-white hover:text-customPurple text-white py-2 px-4 rounded`}>
                             Register as mentor/volunteer
                         </button>
                     </div>
@@ -77,7 +53,7 @@ function Landing() {
                     <Image src={PlaceHoldImage} width={600} alt="image"/>
                 </div>
                 <div className='pt-10  flex justify-center'>
-                    <div className={`w-[70vw] ${isSmallScreen ? '': 'flex'}  text-center justify-around mb-10`}>
+                    <div className={`w-[70vw] ${isMobileScreen ? '': 'flex'}  text-center justify-around mb-10`}>
                         <div className='flex flex-col text-white mb-5'>
                             <p className='text-5xl mb-2'>24</p>
                             <p>hours</p>
