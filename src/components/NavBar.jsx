7f74b67b7fa74c90fb28c4a29db9066fcc27703e
ChrisFong604 <ccffoonngg@gmail.com>
2024-04-13 02:43:05 -0700

    widened global grid center column, updated navbar to be unfixed

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,26 +5,24 @@ import Image from 'next/image';
 function NavBar() {
 
     return (
-        <div>
-        <div className='pb-5 flex pt-5 justify-between text-white ' style={{ position: 'fixed', top: 0, width: '', zIndex: 100 }}>
-            <ul className=' flex items-center ' >
-                <Image width={40} src={PlaceHoldImage2}/>
-              
-                <li className='ml-5 mr-5 hover:underline'>Home</li>
-                <li className='mr-5 hover:underline'>About</li>
-                <li className='mr-5 hover:underline'>Sponsors</li>
-                <li className='mr-5 hover:underline'>FAQ</li>
-                <li className='mr-5 hover:underline'>Surge</li>
-            </ul>
-            <div className='ml-8 flex mr-8 justify-center'>
+        <div className="flex items-center content-center py-10">
+            <div className='py-5 flex justify-between text-white ' style={{ top: 0, width: '', zIndex: 100 }}>
+                <ul className='flex items-center' >
+                    <Image width={40} src={PlaceHoldImage2}/>
+                
+                    <li className='ml-5 mr-5 hover:underline'>Home</li>
+                    <li className='mr-5 hover:underline'>About</li>
+                    <li className='mr-5 hover:underline'>Sponsors</li>
+                    <li className='mr-5 hover:underline'>FAQ</li>
+                    <li className='mr-5 hover:underline'>Surge</li>
+                </ul>
+            </div>
+            <div className='flex ml-auto'>
                 <Image width={40} src={PlaceHoldImage2}/>
                 <Image width={40} src={PlaceHoldImage2}/>
                 <Image width={40} src={PlaceHoldImage2}/>
                 <Image width={40} src={PlaceHoldImage2}/>
             </div>
-            
-        </div>
-
         </div>
     )
 }
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -31,7 +31,7 @@ function Landing() {
     };
 
     return (
-            <div className="[background:linear-gradient(180deg,_#241b38,_#231f20)] grid grid-cols-[1fr_min(75ch,_calc(100%-64px))_1fr] gap-x-[32px] [&>*]:col-span-1 [&>*]:col-start-2 desktop:gap-x-[7.5vh] mobile:gap-x-[5vh] place-content-center">
+        <>
             {!isSmallScreen && <NavBar />}
             {isSmallScreen && 
                 
@@ -56,48 +56,50 @@ function Landing() {
             </div>
             }
             
-            <div className={`${isSmallScreen ? 'items-start pt-32' : 'items-center h-[40vh] pt-24 pl-5'} flex flex-col bg-customPurple justify-center text-white`}>
+            <div>
+                <div className={`${isSmallScreen ? 'items-start pt-32' : 'items-center h-[40vh] pt-24 pl-5'} flex flex-col bg-customPurple justify-center text-white`}>
 
-               
-                <h2>May 18–19, 2024 @ sfu burnaby</h2>
-                <h1 className='text-5xl mt-1 mb-5 '>StormHacks 2024</h1>
-                <p className={`${isSmallScreen ? '' : 'text-center'} text-xs justify-center max-w-sm`}>Lorem ipsum dolor sit amet consectetur adipiscing elit lorem ipsum dolor sit amet consectetur adipiscing elit.</p>
-                <div className={`${isSmallScreen ? 'w-full' : ''} mt-6 `}>
                 
-                    <button className={`${isSmallScreen ? 'w-full mb-5 min-w-[200px]' : 'mr-5'} border border-transparent hover:border-customPurple text-customPurple bg-white  py-2 px-4 rounded`}>
-                        Register as hacker
-                    </button>
-                    <button className={`${isSmallScreen ? 'w-full mb-10 min-w-[200px]' : '' } border hover:bg-white hover:text-customPurple text-white py-2 px-4 rounded`}>
-                        Register as mentor/volunteer
-                    </button>
-                </div>
-            </div>
-            <div className=' h-[51vh] flex justify-center items-center text-white'>
-                <Image src={PlaceHoldImage} width={600} alt="image"/>
-            </div>
-            <div className='pt-10  flex justify-center'>
-                <div className={`w-[70vw] ${isSmallScreen ? '': 'flex'}  text-center justify-around mb-10`}>
-                    <div className='flex flex-col text-white mb-5'>
-                        <p className='text-5xl mb-2'>24</p>
-                        <p>hours</p>
-                    </div>
-                    <div className='flex flex-col text-white mb-5'>
-                        <p className='text-5xl mb-2'>400+</p>
-                        <p>total hackers</p>
-                    </div>
-                    <div className='flex flex-col text-white mb-5'>
-                        <p className='text-5xl mb-2'>$90k</p>
-                        <p>in prizes</p>
-                    </div>
-                    <div className='flex flex-col text-white mb-5'>
-                        <p className='text-5xl mb-2'>90</p>
-                        <p>diverse projects</p>
-                    </div>
+                    <h2>May 18–19, 2024 @ sfu burnaby</h2>
+                    <h1 className='text-5xl mt-1 mb-5 '>StormHacks 2024</h1>
+                    <p className={`${isSmallScreen ? '' : 'text-center'} text-xs justify-center max-w-sm`}>Lorem ipsum dolor sit amet consectetur adipiscing elit lorem ipsum dolor sit amet consectetur adipiscing elit.</p>
+                    <div className={`${isSmallScreen ? 'w-full' : ''} mt-6 `}>
                     
+                        <button className={`${isSmallScreen ? 'w-full mb-5 min-w-[200px]' : 'mr-5'} border border-transparent hover:border-customPurple text-customPurple bg-white  py-2 px-4 rounded`}>
+                            Register as hacker
+                        </button>
+                        <button className={`${isSmallScreen ? 'w-full mb-10 min-w-[200px]' : '' } border hover:bg-white hover:text-customPurple text-white py-2 px-4 rounded`}>
+                            Register as mentor/volunteer
+                        </button>
+                    </div>
+                </div>
+                <div className=' h-[51vh] flex justify-center items-center text-white'>
+                    <Image src={PlaceHoldImage} width={600} alt="image"/>
+                </div>
+                <div className='pt-10  flex justify-center'>
+                    <div className={`w-[70vw] ${isSmallScreen ? '': 'flex'}  text-center justify-around mb-10`}>
+                        <div className='flex flex-col text-white mb-5'>
+                            <p className='text-5xl mb-2'>24</p>
+                            <p>hours</p>
+                        </div>
+                        <div className='flex flex-col text-white mb-5'>
+                            <p className='text-5xl mb-2'>400+</p>
+                            <p>total hackers</p>
+                        </div>
+                        <div className='flex flex-col text-white mb-5'>
+                            <p className='text-5xl mb-2'>$90k</p>
+                            <p>in prizes</p>
+                        </div>
+                        <div className='flex flex-col text-white mb-5'>
+                            <p className='text-5xl mb-2'>90</p>
+                            <p>diverse projects</p>
+                        </div>
+                        
 
+                    </div>
                 </div>
             </div>
-            </div>
+        </>
        
     )
 }
