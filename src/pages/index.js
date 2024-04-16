266a70605e3cd7193f1b7c359b1d0beb6a2ee75b
ChrisFong604 <ccffoonngg@gmail.com>
2024-04-16 00:58:23 -0700

    added mlh banner

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,8 +7,9 @@ import Footer from "./Footer";
 import AboutUs from "../components/AboutUs";
 import NavBar from "../components/NavBar";
 import WhySponsorUs from "../components/WhySponsorUs";
-import FAQ from "../components/FAQ"
+import FAQ from "../components/FAQ";
 import useMobileScreen from "../utils/hooks/useMobileScreen.tsx";
+import { MLHBanner2024 } from "../components/MLHBanner.jsx";
 
 export async function getStaticProps({ locale }) {
   return {
@@ -24,8 +25,9 @@ export default function Home() {
 
   return (
     <div className="[background:linear-gradient(180deg,_#241b38,_#231f20)] mobile:px-4 grid grid-cols-[1fr_min(115ch,_calc(100%-64px))_1fr] gap-x-[32px] [&>*]:col-span-1 [&>*]:col-start-2 desktop:gap-x-[7.5vh] mobile:gap-x-[5vh] place-content-center">
+      <MLHBanner2024 />
       <NavBar isMobileScreen={isMobileScreen} />
-      <Landing isMobileScreen={isMobileScreen}/>
+      <Landing isMobileScreen={isMobileScreen} />
       <AboutUs />
       <TracksThemes />
       <WhySponsorUs />
