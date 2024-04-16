5c96bcbf202534fb802b4efa31cbb4945b3b38db
ChrisFong604 <ccffoonngg@gmail.com>
2024-04-15 22:16:11 -0700

    refactored about us page to include designerfriendly and registration buttons

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,8 +5,6 @@ import SponsorsGallery from "../components/Sponsors/SponsorsGallery.tsx";
 import TracksThemes from "./TracksThemes/TracksThemes";
 import Footer from "./Footer";
 import AboutUs from "../components/AboutUs";
-import DesignerFriendly from "../components/DesignerFriendly";
-import Register from "../components/Register";
 import NavBar from "../components/NavBar";
 import WhySponsorUs from "../components/WhySponsorUs";
 import FAQ from "../components/FAQ"
@@ -25,12 +23,10 @@ export default function Home() {
   const isMobileScreen = useMobileScreen();
 
   return (
-    <div className="[background:linear-gradient(180deg,_#241b38,_#231f20)] grid grid-cols-[1fr_min(115ch,_calc(100%-64px))_1fr] gap-x-[32px] [&>*]:col-span-1 [&>*]:col-start-2 desktop:gap-x-[7.5vh] mobile:gap-x-[5vh] place-content-center">
+    <div className="[background:linear-gradient(180deg,_#241b38,_#231f20)] mobile:px-4 grid grid-cols-[1fr_min(115ch,_calc(100%-64px))_1fr] gap-x-[32px] [&>*]:col-span-1 [&>*]:col-start-2 desktop:gap-x-[7.5vh] mobile:gap-x-[5vh] place-content-center">
       <NavBar isMobileScreen={isMobileScreen} />
       <Landing isMobileScreen={isMobileScreen}/>
       <AboutUs />
-      <DesignerFriendly />
-      <Register />
       <TracksThemes />
       <WhySponsorUs />
       <SponsorsGallery />
