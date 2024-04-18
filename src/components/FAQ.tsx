60f3c346d92afcf35984bdde7306f69ab37d7f20
drlowe <david_lowe@sfu.ca>
2024-04-18 14:16:24 +0800

    faq page fixed with extra div

--- src/components/FAQ.tsx
+++ src/components/FAQ.tsx
@@ -1,25 +1,38 @@
 import React from "react";
-import Accordion from "./Accordion";
 import { FrequentlyAskedQuestions } from "@/manifest";
+import Accordion from "./Accordion";
 
 const FAQ: React.FC = () => {
   return (
-    <>
-      <h1
-        className="pb-8 text-center text-[40px] text-white leading-[100%] font-vollkorn"
-        style={{ marginBottom: "20px" }}
-        id="faq"
-      >
-        Frequently asked <br className="hidden mobile:flex" />
-        questions <br className="hidden mobile:flex" />
-      </h1>
+    <section className="pt-20 self-stretch flex flex-col items-center py-0 px-5 box-border max-w-full text-center text-base text-white font-vollkorn">
+      <div className="w-full max-w-[880px] flex flex-col items-center justify-start gap-40px">
+        <div className="self-stretch flex flex-col items-start justify-start gap-25px">
+          <h1
+            className={`m-0 self-stretch relative text-[40px] leading-[100%] font-normal font-vollkorn mobile:text-center`}
+            style={{ marginBottom: "20px" }}
+          >
+            Frequently Asked <br className="hidden mobile:flex" />
+            Questions <br className="hidden mobile:flex" />
+          </h1>
+        </div>
 
-      <section className="grid desktop:grid-cols-2 mobile:grid-cols-1 row-auto place-content-center desktop:gap-6 mobile:gap-4 desktop:px-24 w-full">
-        {FrequentlyAskedQuestions.map((data, index) => (
-          <Accordion title={data.title} answer={data.answer} key={index} />
-        ))}
-      </section>
-    </>
+        <div className="w-full flex justify-center">
+          <div
+            className={`grid gap-4 mobile:grid-cols-1 desktop:grid-cols-2 w-full max-w-[880px]`}
+          >
+            {FrequentlyAskedQuestions.map((data, index) => (
+              <div className="p-0">
+                <Accordion
+                  title={data.title}
+                  answer={data.answer}
+                  key={index}
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
