cb7e0875f346c0beb22da4dc600590695f4c597a
ChrisFong604 <ccffoonngg@gmail.com>
2024-04-27 00:56:22 -0700

    add faq to desktop nav link

--- src/components/FAQ.tsx
+++ src/components/FAQ.tsx
@@ -3,37 +3,36 @@ import { FrequentlyAskedQuestions } from "@/manifest";
 import Accordion from "./Accordion";
 
 const FAQ: React.FC = () => {
-  return (
-    <section className="pt-5 self-stretch flex flex-col items-center py-0 px-5 box-border max-w-full text-center text-base text-white font-vollkorn">
-      <div className="w-full max-w-[880px] flex flex-col items-center justify-start gap-40px">
-        <div className="self-stretch flex flex-col items-start justify-start gap-25px">
-          <h1
-            className={`m-0 self-stretch relative text-[40px] leading-[100%] font-normal font-vollkorn mobile:text-center`}
-            style={{ marginBottom: "20px" }}
-          >
-            Frequently asked <br className="hidden mobile:flex" />
-            questions <br className="hidden mobile:flex" />
-          </h1>
-        </div>
+	return (
+		<section
+			id="faq"
+			className="pt-5 self-stretch flex flex-col items-center py-0 px-5 box-border max-w-full text-center text-base text-white font-vollkorn"
+		>
+			<div className="w-full max-w-[880px] flex flex-col items-center justify-start gap-40px">
+				<div className="self-stretch flex flex-col items-start justify-start gap-25px">
+					<h1
+						className={`m-0 self-stretch relative text-[40px] leading-[100%] font-normal font-vollkorn mobile:text-center`}
+						style={{ marginBottom: "20px" }}
+					>
+						Frequently asked <br className="hidden mobile:flex" />
+						questions <br className="hidden mobile:flex" />
+					</h1>
+				</div>
 
-        <div className="w-full flex justify-center">
-          <div
-            className={`grid gap-4 mobile:grid-cols-1 desktop:grid-cols-2 w-full max-w-[880px]`}
-          >
-            {FrequentlyAskedQuestions.map((data, index) => (
-              <div className="p-0" key={index}>
-                
-                <Accordion
-                  title={data.title}
-                  answer={data.answer}
-                />
-              </div>
-            ))}
-          </div>
-        </div>
-      </div>
-    </section>
-  );
+				<div className="w-full flex justify-center">
+					<div
+						className={`grid gap-4 mobile:grid-cols-1 desktop:grid-cols-2 w-full max-w-[880px]`}
+					>
+						{FrequentlyAskedQuestions.map((data, index) => (
+							<div className="p-0" key={index}>
+								<Accordion title={data.title} answer={data.answer} />
+							</div>
+						))}
+					</div>
+				</div>
+			</div>
+		</section>
+	);
 };
 
 export default FAQ;
