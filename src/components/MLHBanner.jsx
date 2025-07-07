b969022c41e910e90ad39099c061679f01341b89
brendanshen24 <91840687+brendanshen24@users.noreply.github.com>
2025-07-07 09:22:08 -0700

    added 2026 mlh banner and purebuttons logo

--- src/components/MLHBanner.jsx
+++ src/components/MLHBanner.jsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 
-export const MLHBanner2024 = () => {
+export const MLHBanner2026 = () => {
   return (
     <>
       <a
@@ -15,12 +15,12 @@ export const MLHBanner2024 = () => {
           width: "10%",
           zIndex: "10000",
         }}
-        href="https://mlh.io/na?utm_source=na-hackathon&utm_medium=TrustBadge&utm_campaign=2024-season&utm_content=white"
+        href="https://mlh.io/na?utm_source=na-hackathon&utm_medium=TrustBadge&utm_campaign=2026-season&utm_content=white"
         target="_blank"
       >
         <img
-          src="https://s3.amazonaws.com/logged-assets/trust-badge/2024/mlh-trust-badge-2024-white.svg"
-          alt="Major League Hacking 2024 Hackathon Season"
+          src="https://s3.amazonaws.com/logged-assets/trust-badge/2026/mlh-trust-badge-2026-white.svg"
+          alt="Major League Hacking 2025 Hackathon Season"
           style={{ width: "100%" }}
         />
       </a>
